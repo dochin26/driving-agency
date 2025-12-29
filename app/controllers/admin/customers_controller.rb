c4a0ed775e9910f98f169d720b99628149ccc491
dochin26 <yuuta.2635@gmail.com>
2025-12-29 12:44:51 +0900

    feat: 顧客・車・店舗のCRUDを実装

---
+++ app/controllers/admin/customers_controller.rb
@@ -0,0 +1,72 @@
+module Admin
+  class CustomersController < ApplicationController
+    before_action :authenticate_driver!
+    before_action :authorize_admin!
+    before_action :set_customer, only: [ :show, :edit, :update, :destroy ]
+
+    def index
+      @customers = Customer.all.order(:name)
+    end
+
+    def show
+    end
+
+    def new
+      @customer = Customer.new
+    end
+
+    def create
+      @customer = Customer.new(customer_params)
+
+      respond_to do |format|
+        if @customer.save
+          format.html { redirect_to admin_customers_path, notice: "顧客を登録しました。" }
+          format.turbo_stream { redirect_to admin_customers_path, notice: "顧客を登録しました。" }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.turbo_stream { render :new, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    def edit
+    end
+
+    def update
+      respond_to do |format|
+        if @customer.update(customer_params)
+          format.html { redirect_to admin_customers_path, notice: "顧客を更新しました。" }
+          format.turbo_stream { redirect_to admin_customers_path, notice: "顧客を更新しました。" }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.turbo_stream { render :edit, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    def destroy
+      @customer.destroy
+
+      respond_to do |format|
+        format.html { redirect_to admin_customers_path, notice: "顧客を削除しました。" }
+        format.turbo_stream { redirect_to admin_customers_path, notice: "顧客を削除しました。" }
+      end
+    end
+
+    private
+
+    def set_customer
+      @customer = Customer.find(params[:id])
+    end
+
+    def customer_params
+      params.require(:customer).permit(:name, :address, :latitude, :longitude, :phone, :notes)
+    end
+
+    def authorize_admin!
+      unless current_driver.admin?
+        redirect_to root_path, alert: "管理者権限が必要です。"
+      end
+    end
+  end
+end
