ee18f9a20d78297e8d0afcaf92e5032ea208f387
dochin26 <yuuta.2635@gmail.com>
2026-01-25 23:28:30 +0900

    feat:  ログイン画面のデザインを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,25 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # ログイン後のリダイレクト先
+  def after_sign_in_path_for(resource)
+    dashboard_path
+  end
+
+  # ログアウト後のリダイレクト先
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
   protected
+
+  # Deviseのパラメータ許可設定
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name, :employee_number ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :name, :employee_number ])
+  end
 
   # Admin権限チェック
   def authorize_admin!
